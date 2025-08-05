d9bc4dc864497592ea88c2f9ba264a5e156b4144
chikaodinwanegwo <63511232+chikaodinwanegwo@users.noreply.github.com>
2025-08-05 14:35:34 +0100

    Update sessionCreation.js

--- sessionCreation.js
+++ sessionCreation.js
@@ -15,6 +15,43 @@ app.use(
   })
 );
 
+
+//storing it in memory
+
+const store = new session.MemoryStore();
+
+app.use(
+  session({
+    secret: "secret-key",
+    resave: false,
+    saveUninitialized: false,
+    store,
+  })
+);
+
+
+//sample of it in use
+const express = require("express");
+const app = express();
+const session = require("express-session");
+app.set('trust proxy', 1)
+const PORT = process.env.PORT || 4001;
+// Create your store below:
+const store = new session.MemoryStore();
+
+
+app.use(
+  session({
+    secret: "D53gxl41G",
+    resave: false,
+    saveUninitialized: false,
+    store,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
